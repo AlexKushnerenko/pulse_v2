8a81ddf7229fd682bfcb89d28fd8cd18021b0aa0
Alex <sanyaba92.ak@gmail.com>
2020-05-15 16:48:13 +0300

    second lesson

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+      speed: 1200,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arrow.svg"</button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_arrow.svg"</button',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            dots: true,
+            arrows: false
+          }
+        }
+      ]
+    });
+});
